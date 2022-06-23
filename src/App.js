4a43e23b3c1699fe9206c7402affc05d64fa596b
Jimmy Tran <j4tran@occourts.org>
2022-06-23 11:45:23 -0700

    Code update

--- src/App.js
+++ src/App.js
@@ -1,25 +1,94 @@
 import logo from './logo.svg';
 import './App.css';
+import { Component, createRef } from "react";
+
+const testData = [
+  {
+    "avatar_url": "https://avatars.githubusercontent.com/u/810438?v=4",
+    "name": "dan",
+    "company": "Facebook",
+  },
+  {
+    "avatar_url": "https://avatars.githubusercontent.com/u/47673723?v=4",
+    "name": "Jimmy",
+    "company": "Twitter ",
+  },
+  {
+    "avatar_url": "https://avatars.githubusercontent.com/u/1674?v=4",
+    "name": "Jason",
+    "company": "Snapchat",
+  },
+  {
+    "avatar_url": "https://avatars.githubusercontent.com/u/100828?v=4",
+    "name": "Sophie Essen",
+    "company": "SETI Institute",
+
+  }
+]
+
+
+const CardList = (props) => (
+  <div>
+    {props.profiles.map(profile => <Card {...profile}/>)}
+  </div>
+);
+
+class Card extends Component {
+  render() {
+    const profile = this.props
+    return (
+      <div className="github-profile">
+        <img src={profile.avatar_url} alt='avatar'/>
+        <div className="info">
+          <div className="name" style={{fontSize:'125%'}}>{profile.name}</div>
+          <div className="company">{profile.company}</div>
+        </div>
+      </div>
+    )
+  }
+}
+
+class Form extends Component{
+  state = {userName: ''}
+  handleSubmit = (event) =>{
+    event.preventDefault();
+    console.log(
+      this.userNameInput.current.value
+    )
+  }
+
+  render(){
+    return(
+      <form onSubmit={this.handleSubmit}>
+        <input type="text" value={this.state.userName} onChange={event => this.setState({ userName: event.target.value })} placeholder="GitHub username" required/>
+        <button>Add card</button>
+      </form>
+    )
+  }
+}
+
+class Main extends Component {
+  state = {profiles: testData};
+  render() {
+    return (
+      <div>
+        <div className="header">{this.props.title}</div>
+        <Form />
+        <CardList profiles={this.state.profiles}/>
+      </div>
+    )
+  }
+}
+
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Main title="The GitHub Cards App" />
     </div>
   );
 }
 
 export default App;
+
---
+++ src/Components/Main.js
@@ -0,0 +1,9 @@
+import { Component } from "react";
+
+class Main extends Component{
+    render(){
+        return <div className="header">{this.props.title}</div>
+    }
+}
+
+export default Main
